bee2c113e76b5935f280dc4888a11227a8507398
Jon Schulman <schulman99@gmail.com>
2018-07-02 15:55:33 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
-# your code goes here
+def begins_with_r(array)
+  is_r = []
+  array.collect do |word|
+    if word.slice(0) == "r"
+      is_r.push("r")
+    end
+  end
+  array.length == is_r.length
+end
+
+def contain_a(array)
+  is_a = []
+  array.collect do |word|
+    if word.chars.include?("a")
+      is_a << word
+    end
+  end
+  is_a
+end
+
+def first_wa(array)
+  array.find do |word|
+    word.slice(0,2) == "wa"
+  end
+end
+
+def remove_non_strings(array)
+  array.select { |word| word.is_a? String }
+end
+
+def count_elements (array)
+  new_arr = []
+  array.collect { |elmnt| new_arr.push(elmnt, array.count(elmnt)) }
+    new_arr = new_arr.uniq
+
+  # hash.each_with_object(Hash.new(0)) { |name, count| count[name] += 1 }
+end
+
+
+def merge_data (arr1, arr2)
+end
+
+# new_hash = Hash.new { |hash, key| hash[key] =  }
